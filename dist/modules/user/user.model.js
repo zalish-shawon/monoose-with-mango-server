a0905ecd77d8036129161a89a039b60b2429baa4
Md Zalish Mahmud <zalish.shawon@gmail.com>
2025-06-22 15:47:20 +0600

    add validation for email, phone, and password fields in user model

--- dist/modules/user/user.model.js
+++ dist/modules/user/user.model.js
@@ -10,15 +10,33 @@ const userSchema = new mongoose_1.Schema({
         type: String,
         required: true,
         unique: true,
+        validate: {
+            validator: function (email) {
+                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+            },
+            message: "Please enter a valid email address",
+        }
     },
     phone: {
         type: String,
         required: true,
         unique: true,
+        validate: {
+            validator: function (phone) {
+                return /^\d{10}$/.test(phone);
+            },
+            message: "Please enter a valid phone number",
+        }
     },
     password: {
         type: String,
         required: true,
+        validate: {
+            validator: function (password) {
+                return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(password);
+            },
+            message: "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, and one number.",
+        }
     },
     role: {
         type: String,
--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -10,15 +10,33 @@ const userSchema = new Schema<IUser>({
         type: String,
         required: true,
         unique: true,
+        validate:{
+            validator: function (email: string) {
+                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+            },
+            message: "Please enter a valid email address",
+        }
     },
     phone: {
         type: String,
         required: true,
         unique: true,
+        validate: {
+            validator: function(phone:string) {
+                return /^\d{10}$/.test(phone);
+            },
+            message: "Please enter a valid phone number",
+        }
     },
     password: {
         type: String,
         required: true,
+        validate: {
+            validator: function(password: string) {
+                return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(password);
+            },
+            message: "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, and one number.",
+        }
     },
     role: {
         type: String,
