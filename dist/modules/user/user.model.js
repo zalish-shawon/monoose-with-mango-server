a1928a7f26948f300d5f283484befb380c428566
Md Zalish Mahmud <zalish.shawon@gmail.com>
2025-06-22 12:49:31 +0600

    implement user model and interface with mongoose schema

---
+++ dist/modules/user/user.model.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const userSchema = new mongoose_1.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ["Admin", "Customer"],
+        default: "Customer",
+    },
+}, {
+    timestamps: true,
+});
---
+++ src/modules/user/user.model.ts
@@ -0,0 +1,30 @@
+import { Schema } from "mongoose";
+import { IUser } from "./user.interface";
+
+const userSchema = new Schema<IUser>({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ["Admin", "Customer"],
+        default: "Customer",
+    },
+}, {
+    timestamps: true,
+})
